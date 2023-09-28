ef789ab62a6a298865ce58cd06330de7a4cc1c22
lipeRefosco] <luisfelipe.refosco@hotmail.com>
2023-09-28 17:57:39 -0300

    style: renaming states

--- app/Components/SectionContact/index.tsx
+++ app/Components/SectionContact/index.tsx
@@ -9,23 +9,27 @@ import Input from "../Input";
 import Section from "../Section";
 import { useState } from "react";
 
+const sectionStyle = {
+	justifyContent: 'center',
+}
+
 export default function SectionContact() {
 
-	const [isChecked, setChecked] = useState<boolean>(false)
+	const [isTermsChecked, setTermsChecked] = useState<boolean>(false)
 
-	const handleChecked = () => {
-		setChecked(!isChecked);
+	const handleTermsChecked = () => {
+		setTermsChecked(!isTermsChecked);
 	}
 
 	return (
-		<Section id="contact">
+		<Section id="contact" style={sectionStyle}>
 			<Heading heading={Headings.h2} align={TextAlign.center}>Oi recruter!</Heading>
 			<p>Preencha o formulário para baixar o meu currículo.</p>
 			<Form>
 				<Input type="text" id="nome" label="Nome:" />
 				<Input type="email" id="email" label="E-mail:" />
-				<Checkbox id="termos" text="Aceita os termos blabla ..." checked={isChecked} onHandleChecked={handleChecked} />
-				<Button disabled={!isChecked} filled={true}>Baixar currículo</Button>
+				<Checkbox id="termos" text="Aceita os termos blabla ..." checked={isTermsChecked} onHandleChecked={handleTermsChecked} />
+				<Button disabled={!isTermsChecked} filled={true}>Baixar currículo</Button>
 			</Form>
 		</Section>
 	)
